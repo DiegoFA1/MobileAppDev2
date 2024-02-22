f76c72132a3de5434759633fd12d6ef9011381f6
Diego Fuentes <w4398818@macmini28.gbccloud.com>
2024-02-22 17:28:02 -0500

    Project Creation

---
+++ MobileApp2/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  MobileApp2
+//
+//  Created by Diego Fuentes on 2024-02-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ MobileApp2/MobileApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  MobileApp2App.swift
+//  MobileApp2
+//
+//  Created by Diego Fuentes on 2024-02-22.
+//
+
+import SwiftUI
+
+@main
+struct MobileApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
