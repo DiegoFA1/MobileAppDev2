91e1648e3fc2b82292bade2796396b4eda3e34ff
Yang Hai <yang.hai029@gmail.com>
2024-02-23 15:28:01 -0500

    add product list screen UI

--- MobileApp2/ProductListScreen.swift
+++ MobileApp2/ProductListScreen.swift
@@ -7,12 +7,103 @@
 
 import SwiftUI
 
-struct ProductListScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class Product: Identifiable {
+    var name: String
+    var price: Double
+    var quantity: Int
+    
+    init(name: String, price: Double, quantity: Int) {
+        self.name = name
+        self.price = price
+        self.quantity = quantity
     }
 }
+
+struct ProductListScreen: View {
+    @State private var newProductName = ""
+    @State private var newProductPrice = ""
+    @State private var productList: [Product] = [
+        Product(name: "Tomato", price: 2.49, quantity: 0),
+        Product(name: "Pineapple", price: 5.99, quantity: 0),
+        Product(name: "Red Pepper", price: 3.49, quantity: 0),
+        Product(name: "Mixed Nuts", price: 22.75, quantity: 0)
+    ]
+    
+    var body: some View {
+        //NavigationView{
+            VStack{
+                // Display All Products
+                List{
+                    ForEach(productList.indices, id: \.self){ index in
+                        let product = productList[index]
+                        HStack(spacing: 10){
+                            Text(product.name)
+                            Spacer()
+                            Text(String(format: "$%.2f", product.price))
+                            Spacer()
+                            Stepper(value:$productList[index].quantity, in:0...100){
+                                Text("\(productList[index].quantity)")
+                            }
+                        }
+                    }
+                }
+                
+                //View Cart Button
+                Button(action: {viewCart()}){
+                    Text("View Cart")
+                    .padding(.trailing)
+                        .font(.system(size: 15))
+                }.padding()
+                
+                // Add New Product Area
+                VStack(alignment: .leading){
+                    Text("Add New Product Below")
+                        .padding(.horizontal)
+                    
+                    TextField("Product Name",text:$newProductName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    HStack{
+                        Text("$")
+                        
+                        Spacer()
+                        
+                        TextField("Price",text:$newProductPrice)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding()
+                        
+                        Button(action: {
+                            if let price = Double(newProductPrice){
+                                addProduct(name: newProductName, price: price)
+                            }
+                        }){
+                            Image(systemName: "plus")
+                                .padding(.trailing)
+                                .font(.system(size: 30))
+                        }.padding()
+                    }
+                }
+                .padding()
+            }
+           
+        }
+                        
+        func addProduct(name: String, price: Double){
+//            let product = Product(name: name, price: price, quantity: 0);
+//            productList.append(product)
+//            newProductName = ""
+//            newProductPrice = ""
+        }
+    
+        func viewCart(){
+            //navigate to cart page
+        }
+}
+                
+                
 
 #Preview {
     ProductListScreen()
 }
+
